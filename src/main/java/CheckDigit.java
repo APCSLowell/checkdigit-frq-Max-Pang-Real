ee9189a932890e88ecd04a3ab695013ade7db576
Max-Pang-Real <mapang@s.sfusd.edu>
2025-04-28 14:25:05 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,25 +1,16 @@
 public class CheckDigit 
 {   
-  /** Returns the check digit for num, as described in part (a).  
-   *  Precondition: The number of digits in num is between one and    
-   *  six, inclusive.  
-   *          num >= 0  
-   */  
+ 
    public static int getCheck(int num) 
    {  
      int sum = 0;
-     for(int i = 1; i <= getNumberOfDigit(num); i++){
+     for(int i = 1; i <= getNumberOfDigits(num); i++){
         sum+=getDigit(num, i) * (8-i);
      }
      return sum%10;
    }
  
-  /** Returns true if numWithCheckDigit is valid, or false    
-   *  otherwise, as described in part (b). 
-   *  Precondition: The number of digits in numWithCheckDigit   
-   *  is between two and seven, inclusive.
-   *                numWithCheckDigit >= 0     
-   */     
+    
    public static boolean isValid(int numWithCheckDigit)    
    {      
      int checkdigit = getCheck(numWithCheckDigit%10);
@@ -31,17 +22,14 @@ public class CheckDigit
      }
    }    
    
-   /** Returns the number of digits in num. */    
+    
    public static int getNumberOfDigits(int num)    
    {      
     if(num < 10)
       return 1;
     return 1 + getNumberOfDigits(num/10);    
    }    
-   
-   /** Returns the nthdigit of num.      
-    *  Precondition: n >= 1 and n <= the number of digits in num     
-    */    
+  
     public static int getDigit(int num, int n)    
     {      
       int pos = getNumberOfDigits(num)-n+1;
